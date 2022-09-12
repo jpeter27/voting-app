afbca51a0ba681d05ad2c082960f3f6fe8388da8
Jero Peter <>
2022-09-12 16:27:18 +0200

    add voting app backend base

---
+++ voting-app-server/src/main/java/de/jeropeter/votingappserver/VotingAppServerApplication.java
@@ -0,0 +1,13 @@
+package de.jeropeter.votingappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingAppServerApplication.class, args);
+	}
+
+}
---
+++ voting-app-server/src/test/java/de/jeropeter/votingappserver/VotingAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jeropeter.votingappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
