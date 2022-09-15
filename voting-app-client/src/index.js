0b8ddd5d9dcbcf1d1f5cb6b9b00eb4b08e208995
Jero Peter <>
2022-09-15 18:18:44 +0200

    Add form to add voting

--- voting-app-client/src/index.js
+++ voting-app-client/src/index.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from "./components/App/App";
-import Voting from "./components/Voting/Voting";
+// React
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import VoteController from "./components/VoteController";
 
-const initialVote = {
-    title: "How is your day?",
-    description: "Tell me: how has your day been so far?",
-    choices: [
-        {id: "choice_1", title: "Good", count: 7},
-        {id: "choice_2", title: "Bad", count: 12},
-        {id: "choice_3", title: "Not sure yet", count: 1}
-    ]
-}
+// Sample data
+const allVotes = [
+    {
+        id: "vote_1",
+        title: "How is your day?",
+        description: "Tell me: how has your day been so far?",
+        choices: [
+            { id: "choice_1", title: "Good", count: 7 },
+            { id: "choice_2", title: "Bad", count: 12 },
+            { id: "choice_3", title: "Not sure yet", count: 1 }
+        ]
+    },
+    {
+        id: "vote_2",
+        title: "Programming languages",
+        description: "What is your preferred language?",
+        choices: [
+            { id: "choice_1", title: "JavaScript", count: 5 },
+            { id: "choice_2", title: "Java", count: 9 },
+            { id: "choice_3", title: "Plain english", count: 17 }
+        ]
+    }
+];
 
-root.render(
+// Render VotingComponent
+ReactDOM.render(
     <App>
-        <Voting vote={initialVote}/>
-    </App>
+        <VoteController initialVotes={allVotes} />
+    </App>,
+    document.getElementById("root")
 );
